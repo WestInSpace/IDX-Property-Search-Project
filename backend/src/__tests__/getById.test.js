173f007d90d7523f1b4c3f2cb30421f7f1e00c80
WestInSpace <westinspace@gmail.com>
2026-08-07 13:44:14 -0400

    test(getById.test): Added backend test to route /api/properties/:id, errors - 404 and 500

--- backend/src/__tests__/getById.test.js
+++ backend/src/__tests__/getById.test.js
@@ -20,26 +20,26 @@ describe('GET /api/properties/:id (get a single property by id)', () => {
 		jest.clearAllMocks();
 	});
 
-	//test get property by id
+	//Test get property by id
 	it('should return a property object when a valid ID matches', async () => {
-		const mockProperty = [{ id: 101, L_ListingID: '101', L_City: 'orlando'}, { id: 111, L_ListingID: '111', L_City: 'tampa'}];
+		const mockProperty = { id: 101, L_ListingID: 'LID101', L_City: 'orlando'};
 
-		pool.query.mockResolvedValueOnce([mockProperty]);
+		pool.query.mockResolvedValueOnce([[mockProperty]]);
 
 		const response = await request(app).get('/api/properties/101');
 
 		expect(response.status).toBe(200);
-		expect(response.body).toEqual({id: 101, L_ListingID: '101', L_City: 'orlando'});
+		expect(response.body).toEqual({id: 101, L_ListingID: 'LID101', L_City: 'orlando'});
 		expect(pool.query).toHaveBeenCalledWith(
 			'SELECT * FROM rets_property WHERE id = ? OR L_ListingID = ?', ['101', '101']
 		);
 	});
 
-	//test get property by L_ListingID fallback
+	//Test get property by L_ListingID fallback
 	it('should return a property object when lookup by L_ListingID', async () => {
-		const mockProperty = [{ id: 101, L_ListingID: 'LID101', L_City: 'orlando'}, { id: 111, L_ListingID: 'LID111', L_City: 'tampa'}];
+		const mockProperty = { id: 101, L_ListingID: 'LID101', L_City: 'orlando'};
 
-		pool.query.mockResolvedValueOnce([mockProperty]);
+		pool.query.mockResolvedValueOnce([[mockProperty]]);
 
 		const response = await request(app).get('/api/properties/LID101');
 
@@ -48,6 +48,29 @@ describe('GET /api/properties/:id (get a single property by id)', () => {
 		expect(pool.query).toHaveBeenCalledWith(
 			'SELECT * FROM rets_property WHERE id = ? OR L_ListingID = ?', ['LID101', 'LID101']
 		);
+	});
+
+	//Test when requested property doesn't exist
+	it('should return 404 when property is not found', async () => {
+
+		pool.query.mockResolvedValueOnce([[]]); //mock returning nothing
+
+		const response = await request(app).get('/api/properties/13')
+
+		expect(response.status).toBe(404);
+		expect(response.body).toEqual({
+			message: 'Property not found, check that the id is valid',
+		});
+	});
+
+	//Test database error
+	it('should return 500 when database error occures', async () => {
+		pool.query.mockRejectedValueOnce(new Error('Database error'));
+
+		const response = await request(app).get('/api/properties/101');
+
+		expect(response.status).toBe(500);
+		expect(response.body).toEqual({ error: 'Database error'});
 	});
 
 });
